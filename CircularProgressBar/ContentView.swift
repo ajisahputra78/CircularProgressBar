a21cf3d73095cc5acaceebd758a22a9919372ad8
ajisahputra78 <ajisaputro.s.kom@gmail.com>
2020-11-14 23:53:29 +0700

    Initial Commit

---
+++ CircularProgressBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CircularProgressBar
+//
+//  Created by Aji_sahputra on 14/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
